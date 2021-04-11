d23e6493fc0130784376ffd4435eae5ae0697dee
dadeaka <davide.acanfora@gmail.com>
2021-04-11 13:45:43 +0200

    Updated the script to make it work with the newest API

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Description
-This Python3 script instantly generates a new Spotify account (with random credentials) pretending to be an older Spotify app (v8.4.38) on an Android device. This is simply achieved by sending forged HTTP requests. The original requests have been intercepted from an old GT-P7500, that is why you will find this particular model in the User-Agent of the requests.
+This Python3 script instantly generates a new Spotify account (with random credentials) pretending to be a Spotify app (currently v8.6.16) installed on an Android device. This is simply achieved by sending forged HTTP requests. The original requests have been intercepted from an emulator of a SM-N976N device, that is why you will find this particular model in the User-Agent of the requests.
 
 **Note:**  I won't test this script often, so if Spotify will change their API specifications this script may not work until updated.
 
--- spotify_generator.py
+++ spotify_generator.py
@@ -9,45 +9,53 @@ def getRandomString(length): #Letters and numbers
 def getRandomText(length): #Chars only
     return "".join(random.choice(string.ascii_lowercase) for i in range(length))
 
-print("==Spotify Account Generator==\n")
-print("Setting up..")
-user = getRandomText(8)
-passw = getRandomString(8)
-email = user+"@"+getRandomText(5)+".com"
-payload = {"creation_point": "client_mobile",
-        "gender": "male",
-        "postal_code": 1,
-        "birth_year": 1995,
-        "username": user,
-        "iagree": 1,
-        "birth_month": 4,
-        "password_repeat": passw,
-        "password": passw,
-        "invitecode": "",
-        "key": "142b583129b2df829de3656f9eb484e6",
-        "platform": "Android-ARM",
-        "email": email,
-        "birth_day": 9,
-        "creation_flow": "client_mobile"}
-headers={"Accept-Encoding": "gzip",
-         "Accept-Language": "it-IT;q=1, en-US;q=0.5",
-         "Connection": "Keep-Alive",
-         "Content-Type": "application/x-www-form-urlencoded",
-         "Host": "www.spotify.com",
-         "User-Agent": "Spotify/8.4.38 Android/25 (GT-P7500)"}
 
-print("Sending request..")
-r = requests.post('https://www.spotify.com/int/xhr/json/sign-up/', headers=headers, data=payload)
+def main():
+    print("==Spotify Account Generator==\n")
+    print("Setting up..")
+    nick = getRandomText(8)
+    passw = getRandomString(8)
+    email = nick+"@"+getRandomText(5)+".com"
+    
+    payload = {"creation_point": "client_mobile",
+            "gender": "male",
+            "birth_year": 1995,
+            "displayname": nick,
+            "iagree": "true",
+            "birth_month": 4,
+            "password_repeat": passw,
+            "password": passw,
+            "key": "142b583129b2df829de3656f9eb484e6",
+            "platform": "Android-ARM",
+            "email": email,
+            "birth_day": 9}
+    
+    headers={"Accept-Encoding": "gzip",
+             "Accept-Language": "en-US",
+             "Connection": "Keep-Alive",
+             "Content-Type": "Content-Type: application/x-www-form-urlencoded",
+             "Host": "spclient.wg.spotify.com",
+             "User-Agent": "Spotify/8.6.16 Android/22 (SM-N976N)",
+             "Spotify-App-Version": "8.6.16",
+             "App-Platform": "Android",
+             "X-Client-Id": getRandomString(32)}
 
-if r.status_code==200:
-    if r.json()['status']==1:
-        print("Account created successfully!")
-        print("User: "+user)
-        print("Password: "+passw)
-        print("Email: "+email)
+    print("Sending request..")
+    r = requests.post('https://spclient.wg.spotify.com/signup/public/v1/account/', headers=headers, data=payload)
+
+    if r.status_code==200:
+        if r.json()['status']==1:
+            print("Account created successfully!")
+            print("Nickname: "+nick)
+            print("Username: "+r.json()["username"])
+            print("Password: "+passw)
+            print("Email: "+email)
+        else:
+            print("Could not create the account.")
+            print("Response error:")
+            print(r.json()['errors'])
     else:
-        print("Could not create the account.")
-        print("Response error:")
-        print(r.json()['errors'])
-else:
-    print("Could not load the page, some errors occurred. Response code:", r.status_code)
+        print("Could not load the page, some errors occurred. Response code:", r.status_code)
+
+if __name__ == "__main__":
+    main()
