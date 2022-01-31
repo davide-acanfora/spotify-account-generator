54fc17eebc585026c00618facc5af911348c8cd7
Daniel Jiménez Cabello <danieljcpya354@gmail.com>
2022-01-31 21:17:27 +0100

    fix file creation error

--- spotify_generator.py
+++ spotify_generator.py
@@ -1,56 +1,67 @@
-import requests, string, random, argparse, sys
+import requests
+import string
+import random
+import argparse
+import sys
 
-def getRandomString(length): #Letters and numbers
-    pool=string.ascii_lowercase+string.digits
+
+def getRandomString(length):  # Letters and numbers
+    pool = string.ascii_lowercase+string.digits
     return "".join(random.choice(pool) for i in range(length))
 
-def getRandomText(length): #Chars only
+
+def getRandomText(length):  # Chars only
     return "".join(random.choice(string.ascii_lowercase) for i in range(length))
+
 
 def generate():
     nick = getRandomText(8)
     passw = getRandomString(12)
     email = nick+"@"+getRandomText(5)+".com"
 
-    headers={"Accept-Encoding": "gzip",
-             "Accept-Language": "en-US",
-             "App-Platform": "Android",
-             "Connection": "Keep-Alive",
-             "Content-Type": "application/x-www-form-urlencoded",
-             "Host": "spclient.wg.spotify.com",
-             "User-Agent": "Spotify/8.6.72 Android/29 (SM-N976N)",
-             "Spotify-App-Version": "8.6.72",
-             "X-Client-Id": getRandomString(32)}
-    
-    payload = {"creation_point": "client_mobile",
-            "gender": "male" if random.randint(0, 1) else "female",
-            "birth_year": random.randint(1990, 2000),
-            "displayname": nick,
-            "iagree": "true",
-            "birth_month": random.randint(1, 11),
-            "password_repeat": passw,
-            "password": passw,
-            "key": "142b583129b2df829de3656f9eb484e6",
-            "platform": "Android-ARM",
-            "email": email,
-            "birth_day": random.randint(1, 20)}
-    
-    r = requests.post('https://spclient.wg.spotify.com/signup/public/v1/account/', headers=headers, data=payload)
+    headers = {"Accept-Encoding": "gzip",
+               "Accept-Language": "en-US",
+               "App-Platform": "Android",
+               "Connection": "Keep-Alive",
+               "Content-Type": "application/x-www-form-urlencoded",
+               "Host": "spclient.wg.spotify.com",
+               "User-Agent": "Spotify/8.6.72 Android/29 (SM-N976N)",
+               "Spotify-App-Version": "8.6.72",
+               "X-Client-Id": getRandomString(32)}
 
-    if r.status_code==200:
-        if r.json()['status']==1:
-            return (True, nick+":"+r.json()["username"]+":"+email+":"+passw)
+    payload = {"creation_point": "client_mobile",
+               "gender": "male" if random.randint(0, 1) else "female",
+               "birth_year": random.randint(1990, 2000),
+               "displayname": nick,
+               "iagree": "true",
+               "birth_month": random.randint(1, 11),
+               "password_repeat": passw,
+               "password": passw,
+               "key": "142b583129b2df829de3656f9eb484e6",
+               "platform": "Android-ARM",
+               "email": email,
+               "birth_day": random.randint(1, 20)}
+
+    r = requests.post(
+        'https://spclient.wg.spotify.com/signup/public/v1/account/', headers=headers, data=payload)
+
+    if r.status_code == 200:
+        if r.json()['status'] == 1:
+            return (True, email+":"+passw)
         else:
-            #Details available in r.json()["errors"]
-            #print(r.json()["errors"])
+            # Details available in r.json()["errors"]
+            # print(r.json()["errors"])
             return (False, "Could not create the account, some errors occurred")
     else:
-        return (False, "Could not load the page. Response code: "+ str(r.status_code))
+        return (False, "Could not load the page. Response code: " + str(r.status_code))
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-n", "--number", help="how many accounts to generate, default is 1", type=lambda x: (int(x) > 0) and int(x) or sys.exit("Invalid number: minimum amount is 1"))
-    parser.add_argument("-o", "--output", help="output file, default prints to the console")
+    parser.add_argument("-n", "--number", help="how many accounts to generate, default is 1",
+                        type=lambda x: (int(x) > 0) and int(x) or sys.exit("Invalid number: minimum amount is 1"))
+    parser.add_argument(
+        "-o", "--output", help="output file, default prints to the console")
     args = parser.parse_args()
 
     N = args.number if args.number else 1
@@ -62,9 +73,8 @@ if __name__ == "__main__":
         result = generate()
         if result[0]:
             print(result[1], file=file)
-            if file is not sys.stdout:
-                print(result[1], file=sys.stdout)
+            hits = open("hits.txt", "a")
+            hits.write(result[1] + "\n")
+            hits.close
         else:
             print(str(i+1)+"/"+str(N)+": "+result[1], file=sys.stdout)
-
-    if file is not sys.stdout: file.close()
